c58509b65fd4fa8beb171a3a270e81eb5061215f
alexiscait142 <chilinski.a@gmail.com>
2020-06-22 15:31:57 -0400

    changed dictionary so elements are specific to each paper

--- web-scraper.py
+++ web-scraper.py
@@ -13,14 +13,19 @@ soup = BeautifulSoup(page.content, 'html.parser')
 container = soup.find('ol', class_='breathe-horizontal') # get container that has results
 results = container.find_all('li', class_='arxiv-result') # get each result li
 
-titles = []
-first_authors = []
-last_authors = []
-summaries = []
-pdfs = []
+# titles = []
+# first_authors = []
+# last_authors = []
+# summaries = []
+# pdfs = []
 
-def add_to_list(column, string):
-    column.append(string.replace('\n', ' ').strip())
+results_list = []
+
+# def add_to_list(column, string):
+#     column.append(string.replace('\n', ' ').strip())
+
+# def fix_string(string):
+#     string.replace('\n', ' ').strip()
 
 for result in results:
     title=result.find('p', class_='title').text # get title
@@ -29,29 +34,51 @@ for result in results:
     last_author=authors.split(', ')[-1] # get last author
     summary=result.find('p', class_='abstract').text.replace('△ Less', ' ').replace('▽ More', ' ').replace('Abstract:', ' ') # get summary/abstract
     pdf=result.find('div', class_='is-marginless').find('p', class_='list-title').find('span').find('a', text='pdf')['href'] # get pdf link
-    add_to_list(titles, title)
-    add_to_list(first_authors, first_author)
-    add_to_list(last_authors, last_author)
-    add_to_list(summaries, summary)
-    add_to_list(pdfs, pdf)
+    
+    paper = {
+        "Title": title.replace('\n', ' ').strip(), 
+        "First Author": first_author.replace('\n', ' ').strip(), 
+        "Last Author": last_author.replace('\n', ' ').strip(), 
+        "Summary": summary.replace('\n', ' ').strip(),
+        "PDF": pdf.replace('\n', ' ').strip()
+    }
+    
+    # add_to_list(titles, title)
+    # add_to_list(first_authors, first_author)
+    # add_to_list(last_authors, last_author)
+    # add_to_list(summaries, summary)
+    # add_to_list(pdfs, pdf)
 
-data = {}
-i = 0
-while i < len(titles):
-    data[titles[i]] = {'First Author': first_authors[i], 'Last Author': last_authors[i], 'Summary': summaries[i], 'PDF': pdfs[i]}
-    i += 1
+    results_list.append(paper)
 
-# data[titles[0]] = {'First Author': first_authors[0], 'Last Author': last_authors[0], 'Summary': summaries[0], 'PDF': pdfs[0]}
+# print(results_list)
 
-print(data)
+# i = 0
+# while i < len(results_list):
+#     print(type(results_list[i].items()))
+#     i += 1
 
-# with open('papers.csv', 'w') as out_file:
-#     headers = [
-#         "Title",
-#         "First Author",
-#         "Last Author",
-#         "Summary",
-#         "PDF Link"
-#     ]
-#     writer = csv.DictWriter(out_file, headers)
-#     writer.writeheader()
+# data_list = []
+# data = {}
+# i = 0
+# while i < len(titles):
+#     data[titles[i]] = {'Title': titles[i], 'First Author': first_authors[i], 'Last Author': last_authors[i], 'Summary': summaries[i], 'PDF': pdfs[i]}
+#     i += 1
+
+# print(data.items())
+
+with open('papers.csv', 'w') as out_file:
+    headers = [
+        "Title",
+        "First Author",
+        "Last Author",
+        "Summary",
+        "PDF"
+    ]
+    writer = csv.DictWriter(out_file, headers)
+    writer.writeheader()
+    i = 0
+    while i < len(results_list):
+        for row in results_list[i]:
+            writer.writerow(row)
+        i += 1
